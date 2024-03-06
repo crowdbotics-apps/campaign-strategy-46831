75bede7d37e88257ecb14b440fda2fce28065ed4
Crowdbotics <team@crowdbotics.com>
2024-03-06 03:29:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "campaign-strategy-46831",
+  url: "https://campaign-strategy-46831.botics.co",
+  api: "https://campaign-strategy-46831.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
